d5b78f0985dde8078a7c6bd9b5d5691b48419f2c
martin <mandujar@fi.uba.ar>
2018-03-25 13:44:20 -0300

    Implementing Quicksort using an iterative approach instead of recursive

--- tp1/quick_sort.py
+++ tp1/quick_sort.py
@@ -3,15 +3,36 @@
 from util_sort import swap
 
 def quick_sort(list_test) :
-	quick_sort_method(list_test, 0, len(list_test) - 1)
+	quick_sort_method_iterative(list_test, 0, len(list_test) - 1)
 	return list_test
-	
+
+
 # Ascending order algorithm
 def quick_sort_method(list_test, start, end) :
 	if ( start < end ):
 		newIndex = partition(list_test, start, end)
 		quick_sort_method(list_test, start, newIndex - 1)
 		quick_sort_method(list_test, newIndex + 1, end)
+
+
+# Ascending order algorithm
+def quick_sort_method_iterative(list_test, start, end) :
+	position_stack = []
+	updateStack(position_stack, start, end)
+	while position_stack:
+		positions = position_stack.pop()
+		start, end = positions[0], positions[1]
+		newIndex = partition(list_test, start, end)
+
+		# Rewritten recursive method to iterative method with stack data structure
+		if start < newIndex - 1:
+			updateStack(position_stack, start, newIndex - 1)
+
+		if newIndex + 1 < end:
+			updateStack(position_stack, newIndex + 1, end)
+
+def updateStack(position_stack, start, end):
+	position_stack.append((start, end))
 
 def partition(list_test, start, end) :
 	pivot = list_test[end]
