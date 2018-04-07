005d9416e48e935169837a52ccd57df2a9661bff
martin <mandujar@fi.uba.ar>
2018-04-07 14:51:54 -0300

    Changed insertion_sort to improve execution time

--- tp1/insertion_sort.py
+++ tp1/insertion_sort.py
@@ -4,9 +4,11 @@ from util_sort import swap
 
 # Ascending order algorithm
 def insertion_sort(list_test) :
-	for x in range(0, len(list_test)):
-		y = x
-		while y > 0 and list_test[y - 1] > list_test[y]:
-			swap(list_test, y, y - 1)
-			y = y - 1
-	return list_test
+    for y in range(1, len(list_test)) :
+        key = list_test[y]
+        x = y - 1
+        while x > 0 and list_test[x] > key :
+            list_test[x + 1] = list_test[x]
+            x = x - 1
+        list_test[x + 1] = key
+    return list_test
