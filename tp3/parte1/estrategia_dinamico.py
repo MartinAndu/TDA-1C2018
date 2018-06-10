345b337b072f71086ef692fb687f14d80b0561f8
Seva Gavrilov <gavrilovseva@gmail.com>
2018-06-10 18:06:12 -0300

    Agregada una estrategia dinámica ineficiente

---
+++ tp3/parte1/estrategia_dinamico.py
@@ -0,0 +1,147 @@
+
+from batalla_naval import Estrategia, Juego
+
+from itertools import combinations
+
+
+def copiar_barcos(barcos):
+    return {k: v.copy() for k, v in barcos.items()}
+
+
+def mover_barcos(barcos, tablero):
+    for barco in barcos:
+        barcos[barco]['posicion'] += 1
+        if barcos[barco]['posicion'] >= len(tablero[barco]):
+            barcos[barco]['posicion'] = 0
+
+
+def posibles_disparos(barcos, m):
+    rs = []
+    for k in barcos:
+        rs.extend([k] * m)
+    return set(combinations(rs, m))
+
+
+def disparos_optimos(disparos, barcos, tablero):
+    for d in disparos:
+        if barcos[d]['vida'] <= 0:
+            # Si alguno de los disparos va a un barco sin vida, corto, porque
+            # seguro hay un set de disparos mejor
+            return False
+    return True
+
+
+def resultado_de_disparos(disparos, barcos, tablero):
+    vida_actual = {b: v['vida'] for b, v in barcos.items()}
+    danio_total = 0
+    for d in disparos:
+        danio_posible = tablero[d][barcos[d]['posicion']]
+        danio_total += max(min(danio_posible, vida_actual[d]), 0)
+        vida_actual[d] -= danio_posible
+    return len([x for x in vida_actual.values() if x > 0]), danio_total
+
+
+def resolver_turno(disparos, barcos, tablero):
+    for d in disparos:
+        barcos[d]['vida'] -= tablero[d][barcos[d]['posicion']]
+    mover_barcos(barcos, tablero)
+
+
+def obtener_posicion(barcos, tablero):
+    s = []
+    for i in sorted(barcos):
+        barco = barcos[i]
+        vida = max(barco['vida'], 0)
+        s.append(str(vida)  + 'v' + str(tablero[i][barco['posicion']]) + 'p')
+    return '|'.join(s)
+
+
+class EstrategiaDinamico2(Estrategia):
+
+    m = None
+    res = None
+
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+        self.callcount = 0
+
+    def optimo_recursivo(self, posicion, disparos_hechos, disparos_posibles,
+                         tablero, barcos):
+
+        if posicion not in self.m:
+            self.m[posicion] = {}
+        if disparos_hechos in self.m[posicion]:
+            return self.m[posicion][disparos_hechos]
+
+        self.callcount += 1
+        if self.callcount > 2000000:
+            print(posicion)
+            raise Exception('El tablero es demasiado grande para mi =(')
+
+        tablero = tablero.copy()
+        barcos = copiar_barcos(barcos)
+        maximo = float('inf')
+        p, danio = resultado_de_disparos(disparos_hechos, barcos, tablero)
+        if danio == 0:
+            return maximo
+        pos = self.m[posicion]
+        if p == 0:
+            pos[disparos_hechos] = p
+            self.res[posicion] = disparos_hechos
+            return p
+        else:
+            # Problema: si o si tengo que basarme en movidas anteriores
+            resolver_turno(disparos_hechos, barcos, tablero)
+            nueva_posicion = obtener_posicion(barcos, tablero)
+            pos[disparos_hechos] = maximo
+            for disparos in disparos_posibles:
+                if not disparos_optimos(disparos, barcos, tablero):
+                    continue
+                k = self.optimo_recursivo(nueva_posicion, disparos, disparos_posibles,
+                                          tablero, barcos)
+
+                if p + k < pos[disparos_hechos]:
+                    pos[disparos_hechos] = p + k
+                    self.res[nueva_posicion] = disparos
+                    if k == 0:
+                        break
+            if self.verbose:
+                print('pos ', posicion, ' puntos despues de disparar', p, ' minimo recursivo', minimo)
+        return self.m[posicion][disparos_hechos]
+
+    def construir_matriz(self, tablero, lanzaderos, barcos):
+        self.m = {}
+        self.res = {}
+
+        disparos_posibles = posibles_disparos(barcos, lanzaderos)
+        if self.verbose:
+            print('disparos posibles: ', disparos_posibles)
+
+        minimo = float('inf')
+        posicion_inicial = obtener_posicion(barcos, tablero)
+        for disparos in disparos_posibles:
+            k = self.optimo_recursivo(posicion_inicial, disparos, disparos_posibles,
+                                      tablero, barcos)
+            if k < minimo:
+                minimo = k
+                self.res[posicion_inicial] = disparos
+
+    def siguiente_turno(self, tablero, lanzaderos, barcos, turno):
+        if not self.res:
+            self.construir_matriz(tablero, lanzaderos, barcos)
+            print('tamaño del resultazo', len(self.res))
+
+        posicion = obtener_posicion(barcos, tablero)
+        disparos = self.res[posicion]
+        if self.verbose:
+            print('Estrategia dinamica para turno: ', turno, ', disparos: ', disparos,
+                  ', minimo recursivo: ', self.m[turno][disparos])
+        return disparos
+
+
+if __name__ == '__main__':
+    import sys
+    sys.setrecursionlimit(10000)
+    estrategia = EstrategiaDinamico2(verbose=False)
+    Juego(tablero_file='tablero2', lanzaderos=4,
+          estrategia=estrategia).jugar()
